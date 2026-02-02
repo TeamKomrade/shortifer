4d12e4c976b76e7b5b763fc85dd3e81b7efeae70
Maksim Chursinov <teamkomrade@gmail.com>
2026-02-02 22:41:05 +0500

    ! proper hash to string

--- internal/handler/short_url_handler.go
+++ internal/handler/short_url_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"net/http"
@@ -40,7 +41,7 @@ func (h ShortURLHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 
 		if url != "" {
 			hash := md5.Sum([]byte(url))
-			shortURL = string(hash[:8])
+			shortURL = hex.EncodeToString(hash[:8])
 
 			(*h.Urls)[shortURL] = url
 			res.WriteHeader(http.StatusCreated)

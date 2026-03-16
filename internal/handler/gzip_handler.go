1e281fff7fdddd2f147b5865022ef431d3d49922
Maksim Chursinov <teamkomrade@gmail.com>
2026-03-17 01:36:15 +0500

    = move accept encoding check

--- internal/handler/gzip_handler.go
+++ internal/handler/gzip_handler.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"compress/gzip"
 	"io"
-	"log"
 	"strings"
 
 	http "net/http"
@@ -16,34 +15,35 @@ type GzipWriter struct {
 
 func WithCompression(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		if strings.Contains(req.Header.Get("Content-Encoding"), "gzip") {
+			gzr, err := gzip.NewReader(req.Body)
+			if err != nil {
+				http.Error(res, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			defer gzr.Close()
+
+			body, err := io.ReadAll(gzr)
+			if err != nil {
+				http.Error(res, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			req.Body = io.NopCloser(strings.NewReader(string(body)))
+		}
+
 		if !strings.Contains(req.Header.Get("Accept-Encoding"), "gzip") {
 			h.ServeHTTP(res, req)
 			return
 		}
 
-		gzw, err := gzip.NewWriterLevel(res, gzip.BestSpeed)
+		res.Header().Set("Content-Encoding", "gzip")
 
-		if err != nil {
-			log.Printf("Gzip error: %v", err)
-			return
-		}
+		gzw := gzip.NewWriter(res)
 		defer gzw.Close()
 
-		gzr, err := gzip.NewReader(req.Body)
-		if err != nil {
-			http.Error(res, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer gzr.Close()
-
-		body, err := io.ReadAll(gzr)
-		if err != nil {
-			http.Error(res, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		req.Body = io.NopCloser(strings.NewReader(string(body)))
-
-		res.Header().Set("Content-Encoding", "gzip")
-		h.ServeHTTP(GzipWriter{ResponseWriter: res, Writer: gzw}, req)
+		h.ServeHTTP(GzipWriter{
+			ResponseWriter: res,
+			Writer:         gzw,
+		}, req)
 	})
 }

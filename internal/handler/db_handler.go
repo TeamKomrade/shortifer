ff6a70200e9e7c6f3dea50fccbd769ebd7cb4d56
Maksim Chursinov <teamkomrade@gmail.com>
2026-04-06 00:19:25 +0500

    ! conn close

--- internal/handler/db_handler.go
+++ internal/handler/db_handler.go
@@ -2,8 +2,6 @@ package handler
 
 import (
 	"context"
-	"database/sql"
-	"time"
 
 	pgx "github.com/jackc/pgx/v5"
 
@@ -22,19 +20,6 @@ func (h DatabaseHandler) PingDatabase(res http.ResponseWriter, req *http.Request
 		return
 	}
 	defer conn.Close(context.Background())
-	db, err := sql.Open("sqlite", h.DatabaseConnString)
-	if err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	defer db.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-	if err = db.PingContext(ctx); err != nil {
-		res.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 
 	res.WriteHeader(http.StatusOK)
 }

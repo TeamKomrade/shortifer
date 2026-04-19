8c6c99a4980ccecc927624936158360b68518779
Maksim Chursinov <teamkomrade@gmail.com>
2026-04-19 16:15:13 +0500

    + add migrations

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,21 +3,44 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "modernc.org/sqlite"
 
 	cfg "github.com/TeamKomrade/shortifer/internal/config"
 	httpserver "github.com/TeamKomrade/shortifer/internal/service/httpserver"
+	"github.com/golang-migrate/migrate/v4"
 )
 
 func main() {
 	flags := cfg.ParseStartupFlags()
+	databaseConnectionString := flags.DatabaseConnString
 
-	db, err := sql.Open("sqlite", "shortifer.db")
+	envDatabaseConnString := os.Getenv("DATABASE_DSN")
+	if envDatabaseConnString != "" {
+		databaseConnectionString = envDatabaseConnString
+	}
+
+	db, err := sql.Open("sqlite", databaseConnectionString)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	migrationsPath := "file://migrations"
+
+	m, err := migrate.New(
+		migrationsPath,
+		databaseConnectionString,
+	)
+	if err != nil {
+		log.Fatal("Error initializing migrate:", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("Error applying migrations:", err)
+	}
+	log.Print("Database migrations applied successfully!")
 
 	err = httpserver.CreateServer(flags)
 	if err != nil {
